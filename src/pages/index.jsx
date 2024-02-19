45719fa011db8087fd0e409c793fd8d9e28f711e
Partha Priyadarshi Das <parthap.das36@gmail.com>
2024-02-19 21:46:36 +0000

    Update background of Home Page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,12 @@ import dynamic from 'next/dynamic';
 import Link from 'next/link';
 import TrendGraph from '@/components/GraphWithTrend';
 import { data1, data2, data3, cardData1, cardData2, cardData3 } from '@/data';
-import { ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid';
-import LandingPageCard from '@/components/LandingPageCard'
+import {
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+} from '@heroicons/react/20/solid';
+import LandingPageCard from '@/components/LandingPageCard';
+import Image from 'next/image';
 
 function Index() {
   const Test = dynamic(() => import('./test-7'), { ssr: false });
@@ -14,8 +18,11 @@ function Index() {
   const cardDetails = [
     cardData1,
     cardData2,
-    cardData3
-  ]
+    cardData3,
+    cardData2,
+    cardData1,
+    cardData3,
+  ];
 
   return (
     <div className="w-full lg:max-w-[1120px] lg:px-5 mx-auto xl:max-w-7xl">
@@ -79,12 +86,26 @@ function Index() {
         </button>
       </div>
 
+      {/* background image */}
+      <div className="absolute top-0 left-0 w-full h-full -z-10 bg-[#f5faff]">
+        <Image
+          src="https://explodingtopics.com/static/images/grid-line.svg"
+          alt=""
+          className="w-full h-full object-cover"
+          fill
+        />
+      </div>
+
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mx-2 sm:mx-auto py-10 pt-8 ">
-        {
-          cardDetails.map((card, index) => (
-            <LandingPageCard key={index} cardTitle={card.title} cardDescription={card.description} graphData={card.graphData} volume={card.volume} />
-          ))
-        }
+        {cardDetails.map((card, index) => (
+          <LandingPageCard
+            key={index}
+            cardTitle={card.title}
+            cardDescription={card.description}
+            graphData={card.graphData}
+            volume={card.volume}
+          />
+        ))}
       </div>
     </div>
   );
